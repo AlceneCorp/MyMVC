9eacb2cba391ea1127de39e782d16f8c521d2537
Alcene <alcene@live.fr>
2025-01-10 11:38:25 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,44 +1,43 @@
 <?php
 
-
-    return
+return
+[
+    'DATABASE' =>
     [
-        'DATABASE' =>
+        'host' =>
         [
-            'host' =>
-            [
-                'value' => 'HOST',
-                'type' => 'text',
-                'description' => '',
-                'readonly' => true
-            ],
-            'username' =>
-            [
-                'value' => 'USERNAME',
-                'type' => 'text',
-                'description' => '',
-                'readonly' => true
-            ],
-            'password' =>
-            [
-                'value' => 'PASSWORD',
-                'type' => 'text',
-                'description' => '',
-                'readonly' => true
-            ],
-            'dbname' =>
-            [
-                'value' => 'DBNAME',
-                'type' => 'text',
-                'description' => '',
-                'readonly' => true
-            ],
-            'charset' =>
-            [
-                'value' => 'utf8',
-                'type' => 'text',
-                'description' => '',
-                'readonly' => true
-            ]
+            'value' => 'HOST',
+            'type' => 'text',
+            'description' => 'L\'hôte du serveur de base de données (ex. : localhost, 127.0.0.1 ou une adresse IP externe)',
+            'readonly' => true
+        ],
+        'username' =>
+        [
+            'value' => 'USERNAME',
+            'type' => 'text',
+            'description' => 'Nom d\'utilisateur pour se connecter à la base de données.',
+            'readonly' => true
+        ],
+        'password' =>
+        [
+            'value' => 'PASSWORD',
+            'type' => 'text',
+            'description' => 'Mot de passe associé au nom d\'utilisateur pour accéder à la base de données.',
+            'readonly' => true
+        ],
+        'dbname' =>
+        [
+            'value' => 'DBNAME',
+            'type' => 'text',
+            'description' => 'Nom de la base de données à utiliser pour la connexion.',
+            'readonly' => true
+        ],
+        'charset' =>
+        [
+            'value' => 'utf8',
+            'type' => 'text',
+            'description' => 'Jeu de caractères à utiliser pour la connexion à la base de données. UTF-8 est recommandé.',
+            'readonly' => true
         ]
-    ];
+    ]
+];
